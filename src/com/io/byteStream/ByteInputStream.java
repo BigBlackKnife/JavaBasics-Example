4f47b9056057a4572dac822195f8fb7521bc2700
blaife <2767026270@qq.com>
2019-10-29 00:10:08 +0800

    字节输出流学习

--- src/com/io/byteStream/ByteInputStream.java
+++ src/com/io/byteStream/ByteInputStream.java
@@ -11,9 +11,14 @@ import java.io.IOException;
  */
 public class ByteInputStream {
     public static void main(String[] args) {
+
+        // 为什么要在外面定义 -- 因为在finally块中需要执行关闭方法
+        FileInputStream fis = null;
+
         try {
             // 1.FileInputStream的创建方式：根据路径和File对象。
-            FileInputStream fis = new FileInputStream("F:\\javaBasicsTest\\test.txt");
+            // 如果路径不存在，则抛出异常 FileNotFoundException
+            fis = new FileInputStream("F:\\javaBasicsTest\\test.txt");
             /*File file = new File("F:\\javaBasicsTest\\test.txt");
             FileInputStream fis = new FileInputStream(file);*/
 
@@ -24,7 +29,7 @@ public class ByteInputStream {
                 code = fis.read(); // Unicode码 0-65535  中文会存在乱码情况
             }
 
-            // 2-2.read方法读取内容:byte[] 参数，读取多个字节存入数据内
+            // 2-2.read方法读取内容:参数 byte[]，读取多个字节存入数据内
             byte[] b = new byte[20];
             // 在文件中读取内容后装入数组内，返回值为读取到的有效字节
             int count = fis.read(b);
@@ -35,22 +40,40 @@ public class ByteInputStream {
                 count = fis.read(b);
             }
 
+            // 2-3.read方法读取内容:参数 byte[] off len，读取多个字节存入数据内
+            // 在文件中读取内容后装入数组内，返回值为读取到的有效字节
+            byte[] b2 = new byte[20];
+            fis.read(b2, 5, 10);
+            System.out.print("b2"+new String(b2));
+
             // 3.available 获取管道中还有多少内容，常用于基本流，不用于高级流
             // 在开头使用和结尾使用结果是不一样的，因为中间有操作字节流
             int len = fis.available();
             System.out.println();
             System.out.println(len);
 
-            // 4.skip方法，跳过几个字节。常用与多线程, 返回值为long
+            // 4.skip方法，跳过几个字节。常用于多线程、并发 返回值为long
             // 可以使用负值，使其反向跳跃。
             long v = fis.skip(-9);
             System.out.println(v);
             code = fis.read();
             System.out.println((char)code);
+
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+
+            // 为什么有异常 -- 因为如果在创建file对象时就异常的话fis是空的
+            try {
+                if (fis != null){
+                    fis.close(); // 关闭流管道
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
         }
     }
 }
--- src/com/io/byteStream/ByteOutputStream.java
+++ src/com/io/byteStream/ByteOutputStream.java
@@ -1,5 +1,10 @@
 package com.io.byteStream;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 /**
  * 字节流学习之字节输出流学习类
  * @author Blaife
@@ -8,6 +13,42 @@ public class ByteOutputStream {
 
     public static void main(String[] args) {
 
-    }
+        FileOutputStream fos = null;
+        try {
+            // 1.创建字节输出流 -- 传递file对象或者路径
+            // 第二个参数 append 表示是否追加，默认为false
+            // 如果文件路径不存在则，创建自动文件
+            fos = new FileOutputStream("F:\\javaBasicsTest\\test.txt", true);
+            /*File file = new File("F:\\javaBasicsTest\\test.txt");
+            FileOutputStream fos = new FileOutputStream("F:\\javaBasicsTest\\test.txt");*/
 
+            // 2-1.写入文件方法write，参数：int
+            fos.write(97);
+            fos.flush(); // 将管道中的内容推入（刷新）到硬盘上,现在可以不写此方法
+
+            // 2-2.写入文件方法write，参数：char
+            fos.write('a');
+
+            // 2-3.写入文件方法write，参数：byte[]
+            // byte[] b = new byte[]{97,98,99,100,101};
+            // 填写汉字还是有乱码的问题
+            String str = "测试字节输入";
+            byte[] b = str.getBytes();
+            fos.write(b);
+
+            System.out.println("输入结束");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if ( fos != null) {
+                    fos.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
