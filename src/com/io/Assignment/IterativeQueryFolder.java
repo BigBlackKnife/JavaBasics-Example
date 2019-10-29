d3270405a5a3ae85df90951a60c01737ef6ef9b9
blaife <2767026270@qq.com>
2019-10-29 17:23:25 +0800

    迭代查询文件夹demo -- 迭代

---
+++ src/com/io/Assignment/IterativeQueryFolder.java
@@ -0,0 +1,37 @@
+package com.io.Assignment;
+
+import java.io.File;
+
+/**
+ * 迭代查询文件夹
+ */
+public class IterativeQueryFolder {
+    // 创建文件及调用方法
+    public static void main(String[] args) {
+        outputChild("F:\\javaBasicsTest");
+    }
+
+    /**
+     * 迭代查询一个文件夹
+     * 先输出文件路径，如果有下级就继续调用自身方法
+     * @param file
+     */
+    public static void outputChild(File file) {
+        System.out.println(file.getPath());
+        if (file.listFiles() != null && file.listFiles().length > 0) {
+            for (File f: file.listFiles() ) {
+                outputChild(f);
+            }
+        }
+    }
+
+    /**
+     * 方法重载
+     * @param filePath
+     */
+    public static void outputChild(String filePath) {
+        File file = new File(filePath);
+        outputChild(file);
+    }
+
+}
