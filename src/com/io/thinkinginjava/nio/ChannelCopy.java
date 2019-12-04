2ddde5d54a5a47d484c5206ba60d17ef9e29bcb8
blaife <2767026270@qq.com>
2019-12-04 20:42:07 +0800

    读写双通道复制内容

--- src/com/io/thinkinginjava/nio/ChannelCopy.java
+++ src/com/io/thinkinginjava/nio/ChannelCopy.java
@@ -11,18 +11,18 @@ import java.nio.channels.FileChannel;
  */
 public class ChannelCopy {
     private static final int BSIZE = 1024;
-
     public static void main(String[] args) throws IOException {
-        System.out.println(args.length);
         if (args.length != 2) {
             System.out.println("arguments: sourcefile destfile");
             System.exit(1);
         }
-
+        // 输入通道
         FileChannel in = new FileInputStream(args[0]).getChannel();
+        // 输出通道
         FileChannel out = new FileOutputStream(args[1]).getChannel();
-
+        // 存储字节
         ByteBuffer buffer = ByteBuffer.allocate(BSIZE);
+        // 进行内容复制
         while (in.read(buffer) != -1) {
             buffer.flip();
             out.write(buffer);
