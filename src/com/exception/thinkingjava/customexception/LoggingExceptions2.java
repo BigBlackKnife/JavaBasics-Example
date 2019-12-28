c942be21e3ab137fd86874450e9ad4e97aaa65ae
blaife <2767026270@qq.com>
2019-12-29 00:02:50 +0800

    异常与记录日志 -- 将日志构建在异常处理程序中

---
+++ src/com/exception/thinkingjava/customexception/LoggingExceptions2.java
@@ -0,0 +1,28 @@
+package com.exception.thinkingjava.customexception;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.logging.Logger;
+
+/**
+ * 将日志构建在异常处理程序中
+ */
+public class LoggingExceptions2 {
+
+    private static Logger logger = Logger.getLogger("LoggerException2");
+
+    static void logException(Exception e) {
+        StringWriter trace = new StringWriter();
+        e.printStackTrace(new PrintWriter(trace));
+        logger.severe(trace.toString());
+    }
+
+    public static void main(String[] args) {
+        try {
+            throw new NullPointerException();
+        } catch (NullPointerException e) {
+            logException(e);
+        }
+    }
+
+}
