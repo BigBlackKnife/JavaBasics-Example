076f1770f841697a3f43f6320ea00eb9dc4e6b01
BigBlackKnife <2767026270@qq.com>
2020-01-15 18:20:14 +0800

    异常链 -- 捕获所有异常

---
+++ src/com/exception/thinkingjava/captureallexception/DynamicFields.java
@@ -0,0 +1,113 @@
+package com.exception.thinkingjava.captureallexception;
+
+/**
+ * 异常链
+ * 在捕获一个异常后抛出另一个异常，把原始异常的信息保存下来
+ * 接受一个cause（因由）对象作为参数，这个cause就用来表示原始异常
+ * 只有Error，Exception，RuntimeExcepion提供了带cause参数的构造器
+ * 如果其他异常链接起来，应该使用initCause()方法而不是构造器
+ */
+
+// 自定义异常
+class DynamicFieldsException extends Exception {}
+
+public class DynamicFields {
+    private Object[][] fields;
+    public DynamicFields(int initialSize) {
+        fields = new Object[initialSize][2];
+        for (int i = 0; i < initialSize; i++) {
+            fields[i] = new Object[]{null, null};
+        }
+    }
+
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        for (Object[] obj : fields) {
+            result.append(obj[0]);
+            result.append(": ");
+            result.append(obj[1]);
+            result.append("\n");
+        }
+        return result.toString();
+    }
+
+    private int hasField(String id) {
+        for (int i = 0; i < fields.length; i++) {
+            if (id.equals(fields[i][0])) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private int getFieldNumber(String id) throws NoSuchFieldException {
+        int fieldNum = hasField(id);
+        if (fieldNum == -1) {
+            throw new NoSuchFieldException();
+        }
+        return fieldNum;
+    }
+
+    private int makeField(String id) {
+        for (int i = 0; i < fields.length; i++) {
+            if (fields[i][0] == null) {
+                fields[i][0] = id;
+                return i;
+            }
+        }
+        Object[][] tmp = new Object[fields.length + 1][2];
+        for (int i = 0; i < fields.length; i++) {
+            tmp[i] = fields[i];
+        }
+        for (int i = fields.length; i < tmp.length; i++) {
+            tmp[i] = new Object[]{null, null};
+        }
+        fields = tmp;
+        return makeField(id);
+    }
+
+    public Object getField(String id) throws NoSuchFieldException {
+        return fields[getFieldNumber(id)][1];
+    }
+
+    public Object setField(String id, Object value) throws DynamicFieldsException {
+        if (value == null) {
+            // 在异常中使用initCause设置原始异常
+            DynamicFieldsException dfe = new DynamicFieldsException();
+            dfe.initCause(new NullPointerException());
+            throw dfe;
+        }
+        int fieldNumber = hasField(id);
+        if (fieldNumber == -1) {
+            fieldNumber = makeField(id);
+        }
+        Object result = null;
+        try {
+            result = getField(id);
+        } catch (NoSuchFieldException e) {
+            throw new RuntimeException(e);
+        }
+        fields[fieldNumber][1] = value;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        DynamicFields df = new DynamicFields(3);
+        System.out.println(df);
+        try {
+            df.setField("d", "A value for d");
+            df.setField("number", 47);
+            df.setField("number2", "48");
+            System.out.println(df);
+            df.setField("d", "A new value for d");
+            df.setField("number3", 11);
+            System.out.println("df: " + df );
+            System.out.println("df.getField(\"d\"): " + df.getField("d"));
+            Object field = df.setField("d", null);
+        } catch (DynamicFieldsException e) {
+            e.printStackTrace();
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+    }
+}
