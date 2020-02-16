2b07aff81cb1b3b25fa094c9ecdc77811291c717
BigBlackKnife <2767026270@qq.com>
2020-02-16 22:29:42 +0800

    输出所有的编码格式

---
+++ src/com/io/thinkinginjava/nio/unit01convertdata/AvailableCharSets.java
@@ -0,0 +1,30 @@
+package com.io.thinkinginjava.nio.unit01convertdata;
+
+import java.nio.charset.Charset;
+import java.util.Iterator;
+import java.util.SortedMap;
+
+/**
+ * 输出所有编码格式
+ */
+public class AvailableCharSets {
+    public static void main(String[] args) {
+        SortedMap<String, Charset> charSets = Charset.availableCharsets();
+        Iterator<String> it = charSets.keySet().iterator();
+        while (it.hasNext()) {
+            String csName = it.next();
+            System.out.print(csName);
+            Iterator aliases = charSets.get(csName).aliases().iterator();
+            if (aliases.hasNext()) {
+                System.out.print(": ");
+            }
+            while (aliases.hasNext()) {
+                System.out.print(aliases.next());
+                if (aliases.hasNext()) {
+                    System.out.print(", ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
