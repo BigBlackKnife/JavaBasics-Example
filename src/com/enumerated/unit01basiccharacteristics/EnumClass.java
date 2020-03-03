845cc5330d367eb2fadf8ad54a1c3d11487b0dc5
BigBlackKnife <2767026270@qq.com>
2020-03-03 18:23:12 +0800

    enumerated - 基本的enum特性

--- src/com/enumerated/unit01basiccharacteristics/EnumClass.java
+++ src/com/enumerated/unit01basiccharacteristics/EnumClass.java
@@ -2,6 +2,7 @@ package com.enumerated.unit01basiccharacteristics;
 
 /**
  * 基本enum特性
+ * ordinal、==、equals、getDeclaringClass、name、valueOf
  */
 enum Shrubbery {
     GROUND, CRAWLING, HANGING
@@ -10,15 +11,22 @@ enum Shrubbery {
 public class EnumClass {
     public static void main(String[] args) {
         for (Shrubbery s : Shrubbery.values()) {
-            System.out.println(s + " ordinal: " + " ");
+            // ordinal：表示声明顺序
+            System.out.println(s + " ordinal: " + s.ordinal());
+            // enum实现了Comparable接口，所以可以使用compareTo
             System.out.print(s.compareTo(Shrubbery.CRAWLING) + " ");
+            // equals：实例间的比较
             System.out.print(s.equals(Shrubbery.CRAWLING) + " ");
+            // ==：编译器自动提供equals()和hashcode()方法
             System.out.println(s == Shrubbery.CRAWLING);
+            // getDeclaringClass：输出所属类
             System.out.println(s.getDeclaringClass());
+            // name：输出实例声明时的名称
             System.out.println(s.name());
             System.out.println("------------------------------------");
         }
         for (String s : "HANGING CRAWLING GROUND".split(" ")) {
+            // valueOf：Enum类的静态方法，根据名称返回相应的enum实例
             Shrubbery shrubbery = Enum.valueOf(Shrubbery.class, s);
             System.out.println(shrubbery);
         }
