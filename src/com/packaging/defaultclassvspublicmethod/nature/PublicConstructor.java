129b946391ba2e3985e5f78c2c6f329e153874d7
BigBlackKnife <2767026270@qq.com>
2020-04-02 23:01:07 +0800

    封装 - 访问修饰词 - 包访问权限的类和public构造器

---
+++ src/com/packaging/defaultclassvspublicmethod/nature/PublicConstructor.java
@@ -0,0 +1,10 @@
+package com.packaging.defaultclassvspublicmethod.nature;
+
+/**
+ * @author Blaife
+ * @description 包访问权限的类 和 public构造器
+ * @data 2020/4/2 22:58
+ */
+class PublicConstructor {
+    public PublicConstructor() {}
+}
---
+++ src/com/packaging/defaultclassvspublicmethod/operate/CreatePackageAcessObject.java
@@ -0,0 +1,15 @@
+package com.packaging.defaultclassvspublicmethod.operate;
+
+
+/**
+ * @author Blaife
+ * @description 包访问权限的类 和 public构造器
+ * @data 2020/4/2 22:59
+ */
+public class CreatePackageAcessObject {
+    public static void main(String[] args) {
+        // 'com.packaging.defaultclassvspublicmethod.nature.PublicConstructor' is not public in
+        // 'com.packaging.defaultclassvspublicmethod.nature'. Cannot be accessed from outside package
+        // new PublicConstructor();
+    }
+}
