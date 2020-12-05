cba50a7f4aff217a099652ce70707ad8b4e7a599
BigBlackKnife <2767026270@qq.com>
2020-12-05 15:31:59 +0800

    泛型 - 泛型擦除 - 边界

---
+++ src/com/generics/generice_border/BasicBounds.java
@@ -0,0 +1,115 @@
+package com.generics.generice_border;
+
+/**
+ * @author Blaife
+ * @description 边界的基本要素
+ * @date 2020/12/5 15:02
+ */
+interface HasColor {
+    java.awt.Color getColor();
+}
+
+interface Weight {
+    int weight();
+}
+
+class WithColor<T extends HasColor> {
+    T item;
+
+    WithColor(T item) {
+        this.item = item;
+    }
+
+    T getItem() {
+        return item;
+    }
+
+    java.awt.Color color() {
+        return item.getColor();
+    }
+}
+
+class Coord {
+    public int x, y, z;
+}
+
+class WithColorCoord<T extends Coord & HasColor> {
+    T item;
+
+    WithColorCoord(T item) {
+        this.item = item;
+    }
+
+    T getItem() {
+        return item;
+    }
+
+    java.awt.Color color() {
+        return item.getColor();
+    }
+
+    int getX() {
+        return item.x;
+    }
+
+    int getY() {
+        return item.y;
+    }
+
+    int getZ() {
+        return item.z;
+    }
+}
+
+class Solid<T extends Coord & HasColor & Weight> {
+    T item;
+
+    Solid(T item) {
+        this.item = item;
+    }
+
+    T getItem() {
+        return item;
+    }
+
+    java.awt.Color color() {
+        return item.getColor();
+    }
+
+    int getX() {
+        return item.x;
+    }
+
+    int getY() {
+        return item.y;
+    }
+
+    int getZ() {
+        return item.z;
+    }
+
+    int weight() {
+        return item.weight();
+    }
+}
+
+class Bounded extends Coord implements HasColor, Weight {
+    @Override
+    public java.awt.Color getColor() {
+        return null;
+    }
+
+    @Override
+    public int weight() {
+        return 0;
+    }
+}
+
+public class BasicBounds {
+    public static void main(String[] args) {
+        Solid<Bounded> solid = new Solid<>(new Bounded());
+        solid.color();
+        solid.getY();
+        solid.weight();
+    }
+}
